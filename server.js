9049c560ea649ede86d3d397d78371c3d5aabf59
TMPela <tmpmalleret@gmail.com>
2021-11-20 16:26:55 -0300

    BannerAgregado_01a

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 1808);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('hbs', engine({
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partiaslDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+
+//Routes
+app.use(require('./routes/index.routes'));
+
+//Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
